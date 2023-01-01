651a4e411db5dc0e58028fa93c677845fe51c516
hikalium <hikalium@hikalium.com>
2023-01-02 01:28:43 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{fs, io};
 use anyhow::anyhow;
 use anyhow::Result;
 use argh::FromArgs;
+use gltf::Node;
 
 #[derive(FromArgs)]
 /// VRM as a Code
@@ -12,11 +13,29 @@ struct Args {
     input: Option<String>,
 }
 
+fn parse_node(node: &Node, depth: usize) -> Result<()> {
+    print!("{:width$}", "", width = depth);
+    println!("name: {:?}", node.name());
+    for c in node.children() {
+        parse_node(&c, depth + 1)?;
+    }
+    Ok(())
+}
+
 fn run(path: &str) -> Result<()> {
     let file = fs::File::open(&path)?;
     let reader = io::BufReader::new(file);
     let gltf = gltf::Gltf::from_reader(reader)?;
-    println!("{:#?}", gltf);
+    for scene in gltf.scenes() {
+        println!(
+            "Scene #{} has {} children",
+            scene.index(),
+            scene.nodes().count(),
+        );
+        for node in scene.nodes() {
+            parse_node(&node, 0)?;
+        }
+    }
     Ok(())
 }
 
