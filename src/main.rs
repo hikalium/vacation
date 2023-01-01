4d5a18de386ccd930e56c19572f5b4c9a72645e8
hikalium <hikalium@hikalium.com>
2023-01-02 02:13:27 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::{fs, io};
-
 use anyhow::anyhow;
 use anyhow::Result;
 use argh::FromArgs;
 use gltf::Node;
 use gltf::Semantic;
+use gltf_json as json;
+use json::validation::Checked::Valid;
+use std::borrow::Cow;
+use std::fs;
+use std::io;
+use std::mem;
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
 
 #[derive(FromArgs)]
 /// VRM as a Code
@@ -12,6 +23,9 @@ struct Args {
     /// path to .vrm file to parse
     #[argh(option)]
     input: Option<String>,
+    /// path to .vrm file to export
+    #[argh(option)]
+    output: Option<String>,
 }
 
 fn parse_node(node: &Node, depth: usize) -> Result<()> {
@@ -23,14 +37,14 @@ fn parse_node(node: &Node, depth: usize) -> Result<()> {
     Ok(())
 }
 
-fn run(path: &str) -> Result<()> {
+fn run_input(path: &str) -> Result<()> {
     let file = fs::File::open(&path)?;
     let reader = io::BufReader::new(file);
     let gltf = gltf::Gltf::from_reader(reader)?;
 
     let file = fs::File::open(&path)?;
     let reader = io::BufReader::new(file);
-    let bin = gltf::binary::Glb::from_reader(reader)?;
+    let _bin = gltf::binary::Glb::from_reader(reader)?;
 
     for scene in gltf.scenes() {
         println!(
@@ -80,10 +94,181 @@ fn run(path: &str) -> Result<()> {
     Ok(())
 }
 
+/// Calculate bounding coordinates of a list of vertices, used for the clipping distance of the model
+fn bounding_coords(points: &[Vertex]) -> ([f32; 3], [f32; 3]) {
+    let mut min = [f32::MAX, f32::MAX, f32::MAX];
+    let mut max = [f32::MIN, f32::MIN, f32::MIN];
+
+    for point in points {
+        let p = point.position;
+        for i in 0..3 {
+            min[i] = f32::min(min[i], p[i]);
+            max[i] = f32::max(max[i], p[i]);
+        }
+    }
+    (min, max)
+}
+
+fn align_to_multiple_of_four(n: &mut u32) {
+    *n = (*n + 3) & !3;
+}
+
+fn to_padded_byte_vector<T>(vec: Vec<T>) -> Vec<u8> {
+    let byte_length = vec.len() * mem::size_of::<T>();
+    let byte_capacity = vec.capacity() * mem::size_of::<T>();
+    let alloc = vec.into_boxed_slice();
+    let ptr = Box::<[T]>::into_raw(alloc) as *mut u8;
+    let mut new_vec = unsafe { Vec::from_raw_parts(ptr, byte_length, byte_capacity) };
+    while new_vec.len() % 4 != 0 {
+        new_vec.push(0); // pad to multiple of four bytes
+    }
+    new_vec
+}
+fn run_output(path: &str) -> Result<()> {
+    let triangle_vertices = vec![
+        Vertex {
+            position: [0.0, 0.5, 0.0],
+            color: [1.0, 0.0, 0.0],
+        },
+        Vertex {
+            position: [-0.5, -0.5, 0.0],
+            color: [0.0, 1.0, 0.0],
+        },
+        Vertex {
+            position: [0.5, -0.5, 0.0],
+            color: [0.0, 0.0, 1.0],
+        },
+    ];
+
+    let (min, max) = bounding_coords(&triangle_vertices);
+
+    let buffer_length = (triangle_vertices.len() * mem::size_of::<Vertex>()) as u32;
+    let buffer = json::Buffer {
+        byte_length: buffer_length,
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        uri: None,
+    };
+    let buffer_view = json::buffer::View {
+        buffer: json::Index::new(0),
+        byte_length: buffer.byte_length,
+        byte_offset: None,
+        byte_stride: Some(mem::size_of::<Vertex>() as u32),
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        target: Some(Valid(json::buffer::Target::ArrayBuffer)),
+    };
+    let positions = json::Accessor {
+        buffer_view: Some(json::Index::new(0)),
+        byte_offset: 0,
+        count: triangle_vertices.len() as u32,
+        component_type: Valid(json::accessor::GenericComponentType(
+            json::accessor::ComponentType::F32,
+        )),
+        extensions: Default::default(),
+        extras: Default::default(),
+        type_: Valid(json::accessor::Type::Vec3),
+        min: Some(json::Value::from(Vec::from(min))),
+        max: Some(json::Value::from(Vec::from(max))),
+        name: None,
+        normalized: false,
+        sparse: None,
+    };
+    let colors = json::Accessor {
+        buffer_view: Some(json::Index::new(0)),
+        byte_offset: (3 * mem::size_of::<f32>()) as u32,
+        count: triangle_vertices.len() as u32,
+        component_type: Valid(json::accessor::GenericComponentType(
+            json::accessor::ComponentType::F32,
+        )),
+        extensions: Default::default(),
+        extras: Default::default(),
+        type_: Valid(json::accessor::Type::Vec3),
+        min: None,
+        max: None,
+        name: None,
+        normalized: false,
+        sparse: None,
+    };
+
+    let primitive = json::mesh::Primitive {
+        attributes: {
+            let mut map = std::collections::HashMap::new();
+            map.insert(Valid(json::mesh::Semantic::Positions), json::Index::new(0));
+            map.insert(Valid(json::mesh::Semantic::Colors(0)), json::Index::new(1));
+            map
+        },
+        extensions: Default::default(),
+        extras: Default::default(),
+        indices: None,
+        material: None,
+        mode: Valid(json::mesh::Mode::Triangles),
+        targets: None,
+    };
+
+    let mesh = json::Mesh {
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        primitives: vec![primitive],
+        weights: None,
+    };
+
+    let node = json::Node {
+        camera: None,
+        children: None,
+        extensions: Default::default(),
+        extras: Default::default(),
+        matrix: None,
+        mesh: Some(json::Index::new(0)),
+        name: None,
+        rotation: None,
+        scale: None,
+        translation: None,
+        skin: None,
+        weights: None,
+    };
+
+    let root = json::Root {
+        accessors: vec![positions, colors],
+        buffers: vec![buffer],
+        buffer_views: vec![buffer_view],
+        meshes: vec![mesh],
+        nodes: vec![node],
+        scenes: vec![json::Scene {
+            extensions: Default::default(),
+            extras: Default::default(),
+            name: None,
+            nodes: vec![json::Index::new(0)],
+        }],
+        ..Default::default()
+    };
+
+    let json_string = json::serialize::to_string(&root).expect("Serialization error");
+    let mut json_offset = json_string.len() as u32;
+    align_to_multiple_of_four(&mut json_offset);
+    let glb = gltf::binary::Glb {
+        header: gltf::binary::Header {
+            magic: *b"glTF",
+            version: 2,
+            length: json_offset + buffer_length,
+        },
+        bin: Some(Cow::Owned(to_padded_byte_vector(triangle_vertices))),
+        json: Cow::Owned(json_string.into_bytes()),
+    };
+    let writer = std::fs::File::create(path).expect("I/O error");
+    glb.to_writer(writer).expect("glTF binary output error");
+
+    Ok(())
+}
 fn main() -> Result<()> {
     let args: Args = argh::from_env();
     if let Some(path) = args.input {
-        run(&path)
+        run_input(&path)
+    } else if let Some(path) = args.output {
+        run_output(&path)
     } else {
         Err(anyhow!("Run vacation --help for more information."))
     }
